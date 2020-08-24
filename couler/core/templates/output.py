30b67b19c84e69de30262255589dc679043630e6
terrytangyuan <terrytangyuan@gmail.com>
2020-08-24 10:21:59 -0400

    Set up GitHub Actions and fix pre-commit checks
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- couler/core/templates/output.py
+++ couler/core/templates/output.py
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 class Output(object):
     def __init__(self, value, is_global=False):
         self.value = value
