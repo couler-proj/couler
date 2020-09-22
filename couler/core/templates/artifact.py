b4a4aae7d24b5c5c6ee5203df67893300644e904
Sean Aubin <sean@proteinqure.com>
2020-09-22 17:27:57 -0400

    add basic s3 class by copying OSS

--- couler/core/templates/artifact.py
+++ couler/core/templates/artifact.py
@@ -37,6 +37,65 @@ class Artifact(object):
         return yaml_output
 
 
+class S3Artifact(Artifact):
+    """
+    OssArtifact build the mapping from local path to remote oss bucekt,
+    user can read/write data from OSS as read/write local file
+    """
+
+    def __init__(
+        self,
+        path,
+        accesskey_id,
+        accesskey_secret,
+        bucket,
+        key=None,
+        endpoint="s3.amazonaws.com",
+        is_global=False,
+    ):
+        self.id = "output-s3-%s" % utils._get_uuid()
+        # path is used for local path
+        self.path = path
+        self.type = "s3"
+        self.is_global = is_global
+
+        if accesskey_secret is None or accesskey_id is None or bucket is None:
+            raise SyntaxError("need to input the correct config for s3")
+
+        self.bucket = bucket
+
+        if key is None:
+            # assume the local path is the same as the path of OSS
+            self.key = path
+        else:
+            self.key = key
+
+        self.endpoint = endpoint
+
+        import couler.argo as couler
+
+        secrets = {"accessKey": accesskey_id, "secretKey": accesskey_secret}
+        # TODO: check this secret exist or not
+        self.secret = couler.create_secret(secrets)
+
+    def to_yaml(self):
+        s3_config = OrderedDict(
+            {
+                "endpoint": self.endpoint,
+                "bucket": self.bucket,
+                "key": self.key,
+                "accessKeySecret": {"name": self.secret, "key": "accessKey"},
+                "secretKeySecret": {"name": self.secret, "key": "secretKey"},
+            }
+        )
+        yaml_output = OrderedDict(
+            {"name": self.id, "path": self.path, "s3": s3_config}
+        )
+        if self.is_global:
+            yaml_output["globalName"] = "global-" + self.id
+        return yaml_output
+
+
 class OssArtifact(Artifact):
     """
     OssArtifact build the mapping from local path to remote oss bucekt,
