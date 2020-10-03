ee72ca02282cf833008028fb37b7f02cb7ee3c2a
Yuan Tang <terrytangyuan@gmail.com>
2020-10-03 11:28:04 -0400

    feat: Support PyTorchJob as a pre-defined Couler step (#70)
    * feat: Support PyTorchJob as a pre-defined Couler step
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Rename and fix typos
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

---
+++ couler/steps/pod_utils.py
@@ -0,0 +1,80 @@
+import copy
+
+import couler.argo as couler
+
+
+def _validate_pod_params(
+    pod_type, allowed_pod_types, image=None, replicas=0, restart_policy=None
+):
+
+    if pod_type not in allowed_pod_types:
+        raise ValueError("Invalid value %s for parameter pod_type." % pod_type)
+    if replicas == 0:
+        raise ValueError("Parameter replicas value should be more than 0.")
+    if image is None:
+        raise ValueError("Parameter image should not be None.")
+    if pod_type == "Master" and replicas > 1:
+        raise ValueError("Master/Chief pod's replicas should be 1.")
+    if restart_policy is None:
+        raise ValueError("Parameter restart_policy should not be None.")
+
+
+def _generate_pod_spec(
+    pod_template,
+    container_template,
+    allowed_pod_types,
+    pod_type=None,
+    image=None,
+    replicas=0,
+    secret=None,
+    command="",
+    resources=None,
+    restart_policy=None,
+):
+
+    _validate_pod_params(
+        pod_type=pod_type,
+        allowed_pod_types=allowed_pod_types,
+        image=image,
+        replicas=replicas,
+        restart_policy=restart_policy,
+    )
+
+    container = copy.deepcopy(container_template)
+    container.update({"image": image, "command": command})
+
+    if secret is not None:
+        secret_envs = couler.states._secrets[secret].to_env_list()
+
+        if "env" not in container.keys():
+            container["env"] = secret_envs
+        else:
+            container["env"].extend(secret_envs)
+
+    if resources is not None:
+        # User-defined resource, should be formatted like
+        # "cpu=1,memory=1024,disk=2048,gpu=1,gpu_type=p100,shared_memory=20480"
+        try:
+            kvs = resources.split(",")
+            print(kvs)
+            limits = {}
+            for kv in kvs:
+                k, v = kv.split("=")
+                if k in ["gpu", "memory", "disk", "shared_memory"]:
+                    v = int(v)
+                elif k == "cpu":
+                    v = float(v)
+
+                limits[k] = v
+
+            resource_limits = {"limits": limits}
+            container["resources"] = resource_limits
+
+        except Exception:
+            raise Exception("Unrecognized resource type %s" % resources)
+
+    pod = copy.deepcopy(pod_template)
+    pod.update({"replicas": replicas, "restartPolicy": restart_policy})
+    pod["template"]["spec"]["containers"].append(container)
+
+    return pod
---
+++ couler/steps/pytorch.py
@@ -0,0 +1,97 @@
+import copy
+import uuid
+
+import pyaml
+
+import couler.argo as couler
+from couler.core import utils
+from couler.steps.pod_utils import _generate_pod_spec
+
+pod_types = {"Master", "Worker"}
+
+container_template = {"name": "pytorch", "image": "", "command": ""}
+
+pod_template = {
+    "replicas": 1,
+    "restartPolicy": "",
+    "template": {"spec": {"containers": []}},
+}
+
+manifest_template = {
+    "apiVersion": '"kubeflow.org/v1"',
+    "kind": '"PyTorchJob"',
+    "metadata": {"name": ""},
+    "spec": {"cleanPodPolicy": "", "pytorchReplicaSpecs": {}},
+}
+
+
+def train(
+    image=None,
+    command="",
+    secret=None,
+    master_image=None,
+    master_resources=None,
+    master_restart_policy="Never",
+    master_command=None,
+    num_workers=0,
+    worker_image=None,
+    worker_resources=None,
+    worker_restart_policy="Never",
+    worker_command=None,
+    clean_pod_policy="Running",
+    timeout=None,
+):
+    name = "pytorch-train-%s" % str(uuid.uuid4())
+    success_condition = "status.pytorchReplicaStatuses.Worker.succeeded > 0"
+    failure_condition = "status.pytorchReplicaStatuses.Worker.failed > 0"
+
+    manifest = copy.deepcopy(manifest_template)
+    manifest["metadata"].update({"name": name})
+    manifest["spec"].update({"cleanPodPolicy": clean_pod_policy})
+
+    master_image = master_image if master_image else image
+    master_command = master_command if master_command else command
+
+    chief_pod = _generate_pod_spec(
+        pod_template,
+        container_template,
+        allowed_pod_types=pod_types,
+        pod_type="Master",
+        image=master_image,
+        replicas=1,
+        secret=secret,
+        command=master_command,
+        resources=master_resources,
+        restart_policy=master_restart_policy,
+    )
+
+    manifest["spec"]["pytorchReplicaSpecs"].update({"Master": chief_pod})
+
+    if num_workers > 0:
+        worker_image = worker_image if worker_image else image
+        worker_command = worker_command if worker_command else command
+
+        worker_pod = _generate_pod_spec(
+            pod_template,
+            container_template,
+            allowed_pod_types=pod_types,
+            pod_type="Worker",
+            image=worker_image,
+            replicas=num_workers,
+            secret=secret,
+            command=worker_command,
+            resources=worker_resources,
+            restart_policy=worker_restart_policy,
+        )
+
+        manifest["spec"]["pytorchReplicaSpecs"].update({"Worker": worker_pod})
+
+    step_name, _ = utils.invocation_location()
+
+    couler.run_job(
+        manifest=pyaml.dump(manifest),
+        success_condition=success_condition,
+        failure_condition=failure_condition,
+        step_name=step_name,
+        timeout=timeout,
+    )
