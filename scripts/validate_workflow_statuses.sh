f59ae6354e3a12c4ec251c6fcbccf87d1f5effbd
Yuan Tang <terrytangyuan@gmail.com>
2020-10-13 15:15:39 -0400

    Increase timeout

--- scripts/validate_workflow_statuses.sh
+++ scripts/validate_workflow_statuses.sh
@@ -25,7 +25,7 @@ function get_workflow_status {
     echo ${wf_status}
 }
 
-for i in {1..20}; do
+for i in {1..100}; do
     WF_STATUS=$(get_workflow_status ${WF_NAME})
 
     if [[ "$WF_STATUS" == "Succeeded" ]]; then
