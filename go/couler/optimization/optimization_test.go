86c28def68ccb9eb264528bba1bbc651ebdb437f
Yuan Tang <terrytangyuan@gmail.com>
2020-12-09 15:45:37 -0500

    fix: Fix incorrect optimization package name (#126)
    * fix: Fix incorrect optimization package name
    
    * Update optimization.go

--- go/couler/optimization/optimization_test.go
+++ go/couler/optimization/optimization_test.go
@@ -1,10 +1,9 @@
-package server_test
+package optimization
 
 import (
 	"testing"
 
 	"github.com/alecthomas/assert"
-	opt "github.com/couler-proj/couler/go/couler/optimization"
 	pb "github.com/couler-proj/couler/go/couler/proto/couler/v1"
 )
 
@@ -22,7 +21,7 @@ func TestComposePass(t *testing.T) {
 	w.Steps = []*pb.Step{&pb.Step{Name: "node1"}}
 
 	// EndNodePass would add a node with name "endnode" at the tail
-	opt := opt.Compose(&EndNodePass{})
+	opt := Compose(&EndNodePass{})
 	w, e := opt.Run(w)
 	a.NoError(e)
 	a.Equal(len(w.Steps), 2)
