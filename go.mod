b153662682e47b2b16edc307dea9c3701ed2ee33
Yan Xu <yancey1989@gmail.com>
2020-12-09 22:18:48 +0800

    feat: Add optimization framework in Go (#124)

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/couler-proj/couler
+
+go 1.15
+
+require (
+	github.com/alecthomas/assert v0.0.0-20170929043011-405dbfeb8e38
+	github.com/alecthomas/colour v0.1.0 // indirect
+	github.com/alecthomas/repr v0.0.0-20201120212035-bb82daffcca2 // indirect
+	github.com/golang/protobuf v1.4.3
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+)
---
+++ go/couler/optimization/optimization.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"fmt"
+
+	pb "github.com/couler-proj/couler/go/couler/proto/couler/v1"
+)
+
+// Pass is the interface of the optimization pass
+type Pass interface {
+	Run(w *pb.Workflow) (*pb.Workflow, error)
+}
+
+// ComposedPass composed a sequence of optimization passes
+type ComposedPass struct {
+	passes []Pass
+}
+
+// Run all optimization passes
+func (c *ComposedPass) Run(w *pb.Workflow) (*pb.Workflow, error) {
+	var err error
+	for i, pass := range c.passes {
+		w, err = pass.Run(w)
+		if err != nil {
+			return nil, fmt.Errorf("optimization failed on %d-th pass", i)
+		}
+	}
+	return w, nil
+}
+
+// Compose sequence optimization passes
+func Compose(passes ...Pass) *ComposedPass {
+	return &ComposedPass{passes: passes}
+}
