fd687f67532c0d7668561d61a29cf4a3b9583803
typhoonzero <typhoonzero1986@gmail.com>
2020-12-14 15:59:23 +0800

    fix go tests

--- go/couler/optimization/optimization_test.go
+++ go/couler/optimization/optimization_test.go
@@ -10,20 +10,26 @@ import (
 type EndNodePass struct{}
 
 func (p *EndNodePass) Run(w *pb.Workflow) (*pb.Workflow, error) {
-	step := &pb.Step{Name: "endnode"}
-	w.Steps = append(w.Steps, step)
+	stepList := &pb.ConcurrentSteps{
+		Steps: []*pb.Step{{Name: "endnode"}},
+	}
+	w.Steps = append(w.Steps, stepList)
 	return w, nil
 }
 
 func TestComposePass(t *testing.T) {
 	a := assert.New(t)
 	w := &pb.Workflow{}
-	w.Steps = []*pb.Step{&pb.Step{Name: "node1"}}
+	w.Steps = []*pb.ConcurrentSteps{
+		{
+			Steps: []*pb.Step{{Name: "node1"}},
+		},
+	}
 
 	// EndNodePass would add a node with name "endnode" at the tail
 	opt := Compose(&EndNodePass{})
 	w, e := opt.Run(w)
 	a.NoError(e)
 	a.Equal(len(w.Steps), 2)
-	a.Equal(w.Steps[1].Name, "endnode")
+	a.Equal(w.Steps[1].Steps[0].Name, "endnode")
 }
