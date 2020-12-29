58ce0f67ee203d810b805fcd5620c2b61d7621cb
terrytangyuan <terrytangyuan@gmail.com>
2020-12-29 16:08:15 -0500

    wip
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- go/couler/conversion/argo_workflow.go
+++ go/couler/conversion/argo_workflow.go
@@ -41,6 +41,39 @@ func ConvertToArgoWorkflow(workflowPb *pb.Workflow, namePrefix string) (wfv1.Wor
 	return argoWorkflow, nil
 }
 
+func getArgs(step *pb.Step) wfv1.Arguments {
+	var args wfv1.Arguments
+	for _, arg := range step.GetArgs() {
+		switch stepIOType := arg.GetStepIo().(type) {
+		case *pb.StepIO_Parameter:
+			args.Parameters = append(args.Parameters, wfv1.Parameter{
+				Name:             stepIOType.Parameter.GetName(),
+				Value:			  &stepIOType.Parameter.Value,
+				// TODO: This is not in proto def yet.
+				//GlobalName: 		stepIOType.Parameter.GlobalName,
+			})
+		case *pb.StepIO_Artifact:
+			args.Artifacts = append(args.Artifacts, wfv1.Artifact{
+				Name:             stepIOType.Artifact.GetName(),
+				Path:             stepIOType.Artifact.GetLocalPath(),
+				From:             stepIOType.Artifact.GetValue(),
+				// TODO: This is not used internally yet. Implement this when needed.
+				//ArtifactLocation: wfv1.ArtifactLocation{},
+				GlobalName:       stepIOType.Artifact.GetGlobalName(),
+			})
+		//case *pb.StepIO_Stdout:
+		//case nil:
+		default:
+			//return fmt.Errorf("Profile.Avatar has unexpected type %T", x)
+		}
+	}
+	return args
+}
+
+func getInputsAndOutputs(step *pb.Step) (wfv1.Inputs, wfv1.Outputs) {
+	
+}
+
 func createDAGTasks(workflowPb *pb.Workflow, entryPointName string) []wfv1.Template {
 	templates := []wfv1.Template{{Name: entryPointName}}
 	// Convert steps to DAG tasks.
@@ -54,6 +87,7 @@ func createDAGTasks(workflowPb *pb.Workflow, entryPointName string) []wfv1.Templ
 				Name:         seqStep.GetName(),
 				Template:     seqStep.GetTmplName(),
 				Dependencies: seqStep.GetDependencies(),
+				Arguments: getArgs(seqStep),
 			})
 		templates = append(templates, createSingleStepTemplate(seqStep))
 	}
@@ -66,7 +100,8 @@ func createSeqOrParallelSteps(workflowPb *pb.Workflow, entryPointName string) []
 		seqStep := step.Steps[0]
 		templates[0].Steps = append(templates[0].Steps,
 			wfv1.ParallelSteps{
-				Steps: []wfv1.WorkflowStep{{Name: seqStep.GetName(), Template: seqStep.GetTmplName()}}})
+				Steps: []wfv1.WorkflowStep{
+					{Name: seqStep.GetName(), Template: seqStep.GetTmplName(), Arguments: getArgs(seqStep)}}})
 
 		templates = append(templates, createSingleStepTemplate(seqStep))
 	}
@@ -74,7 +109,9 @@ func createSeqOrParallelSteps(workflowPb *pb.Workflow, entryPointName string) []
 }
 
 func createSingleStepTemplate(step *pb.Step) wfv1.Template {
-	template := wfv1.Template{Name: step.TmplName}
+	template := wfv1.Template{
+		Name: step.TmplName,
+		Inputs: }
 	// TODO: Check mutual exclusivity of different specs.
 	if step.GetContainerSpec() != nil || step.GetScript() != "" {
 		containerSpec := step.GetContainerSpec()
