1bfb03abb7131bf83916b4da2fe50cba9ce9fcfd
Yancey1989 <yancey1989@gmail.com>
2020-12-29 22:04:58 +0800

    update build script

---
+++ scripts/test_python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pb_file=./couler/proto/couler_pb2.py
+cp $pb_file /tmp/old_pb2.py
+
+python setup.py proto
+
+pb_diff=$(diff /tmp/old_pb2.py $pb_file)
+if [[ ! -z "$pb_diff" ]]; then
+  echo "should commit generated protobuf files:" $pb_file
+  exit 1
+fi
+
+python setup.py install
+python -m pytest
