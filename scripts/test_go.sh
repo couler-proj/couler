af55a59cc4cb08611889f9e875a2336fc24b22f2
Yan Xu <yancey1989@gmail.com>
2020-12-29 23:44:46 +0800

    feat: Update generated protobuf files (#147)

---
+++ scripts/test_go.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+pb_file=./go/couler/proto/couler/v1/couler.pb.go
+cp $pb_file /tmp/old.pb.go
+
+go generate ./...
+
+pb_diff=$(diff /tmp/old.pb.go $pb_file)
+if [[ ! -z "$pb_diff" ]]; then
+  echo "should commit generated protobuf files:" $pb_file
+  exit 1
+fi
+
+go test ./... -v
