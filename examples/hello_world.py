f7dc86130748f6e3c7589ee8ff8a4e040317516f
orelmosheweinstock <orelweinstock@gmail.com>
2021-06-18 22:11:07 +0300

    added usage example for default submitter

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -7,3 +7,26 @@ couler.run_container(
 
 submitter = ArgoSubmitter()
 result = couler.run(submitter=submitter)
+
+# Instead of passing the submitter each time, you can just set it as default!
+ArgoSubmitter.set_default(submitter)
+
+couler.run_container(
+    image="docker/whalesay",
+    command=["cowsay"],
+    args=["hello world with default submitter"],
+)
+
+result_with_default_submitter = (
+    couler.run()
+)  # no submitter need be passed now!
+
+couler.run_container(
+    image="docker/whalesay",
+    command=["cowsay"],
+    args=["hello world with default submitter 2"],
+)
+
+result_with_default_submitter_2 = (
+    couler.run()
+)  # no submitter need be passed now either!
